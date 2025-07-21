e767faf7b03bf7d8e78adccc40ecbeb04304487c
ayush03ch <ayush030503@gmail.com>
2025-07-21 18:38:27 +0530

    Added resource group, vnet, subnet config, nsg, load balancer and public IP for LB

---
+++ dev.tfvars
@@ -0,0 +1,4 @@
+env_name = "dev"
+location = "eastus"
+vm_count = 2
+vm_size  = "Standard_B1s"
---
+++ main.tf
@@ -0,0 +1,77 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "env_name" {}
+variable "location" {}
+variable "vm_count" {}
+variable "vm_size" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.env_name}"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.env_name}"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "subnet-${var.env_name}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${var.env_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "lb" {
+  name                = "lb-pip-${var.env_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "main" {
+  name                = "lb-${var.env_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb.id
+  }
+}
